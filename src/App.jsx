28ec29e497ce953e68a6e53ca406978c0c0f08bf
Ramchandra Naik <ramchandranaik@kccemsr.edu.in>
2026-02-09 22:44:31 +0530

    Day 1: Initial Setup - React, Tailwind, and Project Structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-black h-screen text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-stax-teal">
+        STAX SETUP COMPLETE
+      </h1>
+    </div>
+  );
+}
+
+export default App;
